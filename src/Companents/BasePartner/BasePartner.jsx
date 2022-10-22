5efafdfdc88db7dac95fd6c9b7a9c89f39a0c2a0
CavidAghayev <cavidaga307@gmail.com>
2022-10-22 21:39:48 +0400

    data

--- src/Companents/BasePartner/BasePartner.jsx
+++ src/Companents/BasePartner/BasePartner.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 function BasePartner({partner}) {
   return (
     <div className="base-partner-box">
-      <div className='base-partner'>
 <img src={partner.image} alt="" />
-    </div>
     </div>
   )
 }
