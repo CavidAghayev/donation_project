80975dd0841945ac00cb36d692b894108b5be44f
CavidAghayev <cavidaga307@gmail.com>
2022-11-19 03:16:37 +0400

    get

--- src/Companents/CommonDonatePay/CommonDonatePay.jsx
+++ src/Companents/CommonDonatePay/CommonDonatePay.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 // import { handleValidate } from '../../utils/index'
 function handleValidate(event, callback){
   event.preventDefault();
@@ -24,6 +24,14 @@ function handleValidate(event, callback){
 
 function CommonDonatePay() {
 
+
+const validateItem = () => {
+  validateRef.current.style.display = "none"
+}
+
+  const validateRef = useRef();
+
+
     const [user,setUser] = useState({
         amount: "",
         name: "",
@@ -38,7 +46,6 @@ function CommonDonatePay() {
 
       const [errors, setErrors] = useState({});
 console.log(errors)
-const validateRef = useRef();
       const onSubmit = ({isError,errors,data}) => {
         setErrors({})
         if(isError){
@@ -52,36 +59,49 @@ const validateRef = useRef();
         setUser(prev => ({...prev, [name] : value }))
       }
 
+      const changeInputAmount = (e) => {
+        let val = e.target.value
+        setUser(prev => ({...prev, [e.target.name] : val}))
+val = val.replace(/\D/g, '')
+      }
+
       const changeInputName = (e) => {
         let val = e.target.value
         setUser(prev => ({...prev, [e.target.name] : val}))
       val = val.charAt(0).toUpperCase() + val.slice(1).toLowerCase();
-
+        val = val.replace(/[^a-zA-Z]+/, '')
       }
 
       const changeInputSurname = (e) => {
         let val = e.target.value
         setUser(prev => ({...prev, [e.target.name] : val}))
       val = val.charAt(0).toUpperCase() + val.slice(1).toLowerCase(); 
+        val = val.replace(/[^a-zA-Z]+/, '')
       }
-
 
       const changeInputCardOwnName = (e) => {
         let val = e.target.value
         setUser(prev => ({...prev, [e.target.name] : val}))
       val = val.charAt(0).toUpperCase() + val.slice(1).toLowerCase(); 
+        val = val.replace(/[^a-zA-Z]+/, '')
       }
 
       const changeInputCardNumber = (e) => {
         let val = e.target.value
         setUser(prev => ({...prev, [e.target.name] : val}))
 val = val.replace(/\D/g, '')
+     if(val.length>0){
+      val = val.match(new RegExp(/.{1,4}/, 'g')).join(" ")
+    }
       }
 
       const changeInputDate = (e) => {
         let val = e.target.value
         setUser(prev => ({...prev, [e.target.name] : val}))
 val = val.replace(/\D/g, '')
+if(val.length>0){
+  val = val.match(new RegExp(/.{1,2}/, 'g')).join("/")
+}
       }
       
       const changeInputCvv = (e) => {
@@ -103,7 +123,7 @@ console.log(user)
 <div className="validate-modal__text">
   <h3>Təşəkkürlər!</h3>
   <h5>Ödənişiniz uğurla təqdim <br /> edildi, Təşəkkürlər!</h5>
-  <div className='btn validate-btn'>Ok</div>
+  <div onClick={validateItem} className='btn validate-btn'>Ok</div>
 </div>
     </div>
     <div data-aos="fade-up" className="donation-content">
@@ -136,8 +156,8 @@ console.log(user)
         </p>
         <form className='form' onSubmit={e => handleValidate(e, onSubmit)} noValidate>
           <label htmlFor="amount">İanə Məbləği *</label>
-          <input onChange={changeInput}  name="amount" id='amount' value={user.amount} required={true} minLength={1} maxLength={7}/>
-          {errors.amount && <p style={{color: "red", fontSize: "12px",margin: "20px", lineHeight: "0px", position: "absolute"}}>Bu tələb olunur</p>}
+          <input  onChange={changeInputAmount}  name="amount" id='amount' placeholder='$' value={user.amount} required={true} minLength={1} maxLength={7}/>
+          {errors.amount && <p style={{color: "red", fontSize: "12px", lineHeight: "0px", position: "absolute"}}>Bu tələb olunur</p>}
           <label htmlFor="name">Ad *</label>
           <input onChange={(e) => changeInputName(e)} type="text" name="name" id="name" value={user.name} pattern="[A-Za-z]+" required={true} minLength={3} maxLength={11}/>
           {errors.name && <p style={{color: "red", fontSize: "12px",  lineHeight: "0px", position: "absolute"}}>Bu tələb olunur</p>}
@@ -145,7 +165,7 @@ console.log(user)
           <input onChange={changeInputSurname} type="text" name="surname" id="surname" value={user.surname} required={true} pattern="[A-Za-z]+" minLength={3} maxLength={11}/>
           {errors.surname && <p style={{color: "red", fontSize: "12px", lineHeight: "0px", position: "absolute"}}>Bu tələb olunur</p>}
           <label htmlFor="email">E-mail *</label>
-          <input onChange={changeInput} type="text" name="email" id="email" value={user.email} required={true} />
+          <input onChange={changeInput} type="text" name="email" id="email" value={user.email} pattern="^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$" required={true} />
           {errors.email && <p  style={{color: "red", fontSize: "12px", lineHeight: "0px", position: "absolute"}}>Bu tələb olunur</p>}
           <label htmlFor="nameofcardholder">Kart Sahibinin Adı *</label>
           <input onChange={changeInputCardOwnName}
@@ -172,7 +192,9 @@ console.log(user)
           />
           <br />
           {errors.cardNumber && <p style={{color: "red", fontSize: "12px", lineHeight: "0px", position: "absolute"}}>Bu tələb olunur</p>}
+          <label htmlFor="date">Tarix *</label>
           <input onChange={changeInputDate}
+          id="date"
             className="date"
             name="date"
             placeholder="mm/yy"
@@ -182,7 +204,9 @@ console.log(user)
         maxLength={5}
           /> 
           {errors.date && <p style={{color: "red", fontSize: "12px", lineHeight: "0px", position: "absolute"}}>Bu tələb olunur</p>}
+           <label   htmlFor="cvv">Cvv *</label>
           <input onChange={changeInputCvv}
+          id="cvv"
             className="cvv"
             name="cvv"
             placeholder="cvv"
